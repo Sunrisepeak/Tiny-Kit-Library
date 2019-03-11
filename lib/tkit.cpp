52b74f123baebe1859b88e0f729b0b88bc60234d
Your Name <you@example.com>
2019-03-12 00:15:54 +0800

    first method random_

--- lib/tkit.cpp
+++ lib/tkit.cpp
@@ -1,10 +1,10 @@
+#include<iostream>
 #include<tkit>
 #include<random>
-using namespace std;
 void number_generator(int a[],int begin, int end, int number)
 {
-	default_random_engine generator;
-	uniform_int_distribution<int> r_number(begin, end);
+	std::default_random_engine generator;
+	std::uniform_int_distribution<int> r_number(begin, end);
 	for(int i = 0; i < number; i++)
 		a[i] = r_number(generator);
 }
