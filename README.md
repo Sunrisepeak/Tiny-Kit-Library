485e4c5fda5c9e6a000f4a1d41883f01d64c3812
Your Name <you@example.com>
2019-03-13 00:47:26 +0800

    tried now

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Tiny-Kit-Library
 a common lib that include more more more method of high-frequency and let we feel more than easy to code.
+
+[![Build Status](https://travis-ci.org/Sunrisepeak/Tiny-Kit-Library.svg?branch=master)](https://travis-ci.org/Sunrisepeak/Tiny-Kit-Library)
+
+## Function
+Random
--- lib/tkit.cpp
+++ lib/tkit.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 #include<tkit>
 #include<random>
-void number_generator(int a[],int begin, int end, int number)
+void TData::rdata_generator(
+		address_pointer address,
+	   	const int mix,
+	   	const int max, 
+		const int x, 
+		const int y, 
+		const int z
+	)
 {
+	const int length = z*x*y;
+	int* it = (int *)address;
 	std::default_random_engine generator;
-	std::uniform_int_distribution<int> r_number(begin, end);
-	for(int i = 0; i < number; i++)
-		a[i] = r_number(generator);
+	std::uniform_int_distribution<int> range_number(mix, max);
+	for(int i = 0; i < length; i++, it++ )
+	{
+		*it = range_number(generator);	
+	}
 }
